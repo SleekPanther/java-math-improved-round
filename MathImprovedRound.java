e38772b281f24f97ac794f2f5bcbc62748fb2298
SleekPanther <macrobyte2@gmail.com>
2017-01-08 09:16:26 -0500

    basic class setup

---
+++ MathImprovedRound.java
@@ -0,0 +1,10 @@
+public class MathImprovedRound {
+
+	public static void main(String[] args) {
+		System.out.println(MathImprovedRound.round());
+	}
+	
+	public static double round(){
+		return 2.0;
+	}
+}
